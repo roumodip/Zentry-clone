d6f14ebeb5fecafb4c3f5293bbebc90da947c9e5
roumodip <roumodipdas456@gmail.com>
2024-12-09 18:48:53 +0530

    little changes

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,6 +1,8 @@
 import { gsap } from "gsap";
 import { useEffect, useRef } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import clsx from "clsx";
+
 
 
 gsap.registerPlugin(ScrollTrigger);
